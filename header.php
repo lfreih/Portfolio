b628ba5f5ff024377ffd2312f40b3af67aa32074
developedbylucie <lfreihaut04@gmail.com>
2026-01-25 10:07:29 +0100

    fix include header and footer

--- header.php
+++ header.php
@@ -1,22 +1,22 @@
-<header class="fixed top-0 w-full z-50 flex justify-between items-center px-6 lg:px-24 py-3 bg-[#98ABEE]/80 backdrop-blur-md font">
+<header class="fixed top-0 w-full z-50 flex justify-between items-center px-6 lg:px-24 py-3 bg-[#98ABEE]/20 backdrop-blur-md ">
     <a href="/" class="flex items-center gap-3">
-        <img src="../images/favicon.svg" alt="logo" class="w-8">
-        <span class="font-bold tracking-tight">Lucie Freihaut</span>
+        <img src="images/code-xml.svg" alt="logo" class="w-8">
+        <span class="font font-semibold tracking-tight">Lucie Freihaut</span>
     </a>
 
-    <nav>
+    <nav class="font">
         <ul class="hidden md:flex gap-8 text-[14px] uppercase tracking-wider font-medium">
-            <li><a href="#" class="relative group py-1">
+            <li><a href="pdf/CV_Lucie_Freihaut.pdf" class="relative group py-1">
                 CV
-                <span class="absolute left-0 bottom-0 w-0 h-0.75 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
+                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
             </a></li>
-            <li><a href="https://www.linkedin.com/in/lucie-freihaut" class="relative group py-1">
+            <li><a href="https://linkedin.com/in/lucie-freihaut" class="relative group py-1">
                 LINKEDIN
-                <span class="absolute left-0 bottom-0 w-0 h-0.75 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
+                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
             </a></li>
             <li><a href="https://github.com/lfreih" class="relative group py-1">
                 GitHub
-                <span class="absolute left-0 bottom-0 w-0 h-0.75 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
+                <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
             </a></li>
         </ul>
     </nav>
--- index.php
+++ index.php
@@ -13,30 +13,7 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body class="">
-    <?php include 'includes/header.php'; ?>
-    <!-- <header class="fixed top-0 w-full z-50 flex justify-between items-center px-6 lg:px-24 py-3 bg-[#98ABEE]/20 backdrop-blur-md ">
-        <a href="/" class="flex items-center gap-3">
-            <img src="images/code-xml.svg" alt="logo" class="w-8">
-            <span class="font font-semibold tracking-tight">Lucie Freihaut</span>
-        </a>
-
-        <nav class="font">
-            <ul class="hidden md:flex gap-8 text-[14px] uppercase tracking-wider font-medium">
-                <li><a href="pdf/CV_Lucie_Freihaut.pdf" class="relative group py-1">
-                    CV
-                    <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
-                </a></li>
-                <li><a href="https://linkedin.com/in/lucie-freihaut" class="relative group py-1">
-                    LINKEDIN
-                    <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
-                </a></li>
-                <li><a href="https://github.com/lfreih" class="relative group py-1">
-                    GitHub
-                    <span class="absolute left-0 bottom-0 w-0 h-0.5 bg-[#1D24CA] transition-all duration-300 group-hover:w-full"></span>
-                </a></li>
-            </ul>
-        </nav>
-    </header> -->
+    <?php include 'header.php'; ?>
 
     <!-- <main class="mt-[20px]"> -->
     <main>
@@ -491,6 +468,6 @@
 
     </main>
 
-    <?php include 'includes/foooter.php'; ?>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
